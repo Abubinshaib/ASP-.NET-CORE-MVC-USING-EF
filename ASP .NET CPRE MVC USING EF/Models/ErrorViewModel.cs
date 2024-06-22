df7aef7ffc69272ef16228acca9e8ad1fe623246
Ameerkhan <110119104009@aalimec.ac.in>
2024-06-22 22:49:22 +0530

    Add project files.

---
+++ ASP .NET CPRE MVC USING EF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_.NET_CPRE_MVC_USING_EF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
